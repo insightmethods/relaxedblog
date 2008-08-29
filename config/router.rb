bd8485f50cc0f4f70063196486b10dc5c95011b2
Niket Patel <email@niket.info>
2008-08-30 03:13:45 +0530

    [CHANGE] configured router

--- config/router.rb
+++ config/router.rb
@@ -23,12 +23,28 @@ Merb.logger.info("Compiling routes...")
 Merb::Router.prepare do |r|
   # RESTful routes
   # r.resources :posts
+  
+  r.match("/publish/:action").
+    to(:controller => "publish")
+    
+  r.match("/", :method => :get).
+    to(:controller => "pages", :action => "index").
+    name(:home)
+    
+  r.match("/:id", :method => :get).
+    to(:controller => "pages", :action => "show").
+    name(:page)
+    
+  r.match("/:id/comment", :method => :post).
+    to(:controller => "pages", :action => "created_comment").
+    name(:page_comments)
+    
 
   # This is the default route for /:controller/:action/:id
   # This is fine for most cases.  If you're heavily using resource-based
   # routes, you may want to comment/remove this line to prevent
   # clients from calling your create or destroy actions with a GET
-  r.default_routes
+  # r.default_routes
   
   # Change this for your home page to be available at /
   # r.match('/').to(:controller => 'whatever', :action =>'index')
